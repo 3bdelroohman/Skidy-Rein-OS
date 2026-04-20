7e78fc40e50b9e490a23bc0ecf7e515a675a02d5
3bdelroohman <3bdelroohman@gmail.com>
2026-04-20 23:16:47 +0200

    chore: remove temporary batch scripts and artifacts

--- _check.js
+++
@@ -1,4 +0,0 @@
-﻿const c = require('fs').readFileSync('src/components/teachers/teacher-form.tsx', 'utf8');
-console.log('Line 12:', c.split('\n')[11]);
-console.log('BOM:', c.charCodeAt(0));
-console.log('Has web:', c.includes('"web"'));
--- _dump_teacher_id.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("src/app/(dashboard)/teachers/[id]/page.tsx", "utf8");
-require("child_process").spawn("clip").stdin.end(c);
-console.log("Copied to clipboard, length:", c.length);
