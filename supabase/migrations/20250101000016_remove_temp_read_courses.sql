476a7813b0ccd0450ad0ae637fc57eb2149ee117
3bdelroohman <3bdelroohman@gmail.com>
2026-04-27 19:51:35 +0300

    chore: remove temporary course read policy

---
+++ supabase/migrations/20250101000016_remove_temp_read_courses.sql
@@ -0,0 +1,9 @@
+﻿-- Migration 016: Remove temporary courses read policy safely
+-- Preconditions:
+-- - courses_select already allows public SELECT.
+-- - Browser testing confirmed course dropdowns still work.
+--
+-- Removed policy:
+-- - temp_read_courses
+
+DROP POLICY IF EXISTS temp_read_courses ON public.courses;
